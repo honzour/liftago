0de02e3743ce0ffd96956445dc31e269db05e1e4
Jan Nemec <honza@debian.honza>
2015-07-01 06:55:55 +0200

    Hello world project with splash screen

---
+++ src/src/com/adleritech/android/developertest/application/SplashScreenActivity.java
@@ -0,0 +1,17 @@
+package com.adleritech.android.developertest.application;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class SplashScreenActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.splash_screen);
+	}
+
+	
+	
+}
