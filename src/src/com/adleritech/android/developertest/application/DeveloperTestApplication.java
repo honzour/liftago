ee8919ae47b84bbd5237e5b9a23178a1877f55de
Jan Nemec <jnemec@acrobits.cz>
2015-07-01 08:50:18 +0200

    ApplicationClass

---
+++ src/src/com/adleritech/android/developertest/application/DeveloperTestApplication.java
@@ -0,0 +1,12 @@
+package com.adleritech.android.developertest.application;
+
+import android.app.Application;
+
+public class DeveloperTestApplication extends Application {
+
+	@Override
+	public void onCreate() {
+		super.onCreate();
+	}
+
+}
